ebb6d11495dcd163bf33ef08009df40ae075b228
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2026-02-03 10:18:42 -0300

    feat: adiciona o class validator para validação do corpo de requisições e melhor documentação dos dtos no swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Pagô - Teste Técnico Backend 2025 Trimestre 4')
