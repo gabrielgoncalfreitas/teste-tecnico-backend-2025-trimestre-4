ebc896d681623a0182545d2d06a26f3b665db217
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2026-02-04 13:23:41 -0300

    chore: refatoração para utilizar apenas o app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { CrawlModule } from './crawl.module';
+import { PrismaService } from './services/prisma.service';
+import { SqsService } from './services/sqs.service';
+import { CrawlService } from './services/crawl.service';
+import { CepCacheService } from './services/cep-cache.service';
+import { AddressService } from './services/address.service';
+import { ViaCepProvider } from './services/address/viacep.provider';
+import { OpenCepProvider } from './services/address/opencep.provider';
+import { CepCrawlCreateHandler } from './handlers/cep.crawl.create.handler';
+import { CepCrawlGetHandler } from './handlers/cep.crawl.get.handler';
+import { CepCrawlResultsHandler } from './handlers/cep.crawl.results.handler';
+import { CepCrawlCreateController } from './controllers/cep.crawl.create.controller';
+import { CepCrawlGetController } from './controllers/cep.crawl.get.controller';
+import { CepCrawlResultsController } from './controllers/cep.crawl.results.controller';
 import { HealthController } from './controllers/health.controller';
+import { CrawlWorker } from './workers/crawl.worker';
 
 @Module({
   imports: [
@@ -9,9 +22,25 @@ import { HealthController } from './controllers/health.controller';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    CrawlModule,
   ],
-  controllers: [HealthController],
-  providers: [],
+  controllers: [
+    CepCrawlCreateController,
+    CepCrawlGetController,
+    CepCrawlResultsController,
+    HealthController,
+  ],
+  providers: [
+    PrismaService,
+    SqsService,
+    CrawlService,
+    CepCacheService,
+    AddressService,
+    ViaCepProvider,
+    OpenCepProvider,
+    CepCrawlCreateHandler,
+    CepCrawlGetHandler,
+    CepCrawlResultsHandler,
+    CrawlWorker,
+  ],
 })
 export class AppModule {}
